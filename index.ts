6122193460d9d1d39f171213f932fba8ab0605bb
curry0312 <curry900312@gmail.com>
2024-10-20 13:52:26 +0800

    Step 2: setup express

---
+++ index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+
+import restaurantRouter from "./routes/restaurant.js";
+import cuisinesRouter from "./routes/cuisines.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/cuisines", cuisinesRouter);
+app.use("/restaurants", restaurantRouter);
+
+app.use(errorHandler);
+
+app
+  .listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  })
+  .on("error", (err) => {
+    console.error(err);
+    throw new Error(err.message);
+  });
